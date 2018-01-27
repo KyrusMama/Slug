fecfa05e60792085358354234d1ff106014f3ecf
Ayaan-P <35133058+Ayaan-P@users.noreply.github.com>
2018-01-27 14:23:31 -0600

    'Calculate'

--- Reflex/src/R_A.java
+++ Reflex/src/R_A.java
@@ -1,8 +1,7 @@
 public class R_A extends IonotropicReceptor{
-<<<<<<< HEAD
-=======
+
     double potIncrease = 10;// growth curve
->>>>>>> b0298e4820eb2fd11af394af1fa0c23c3d5d3fc9
+
     @Override
     public void transmit(Neurotransmitter nt) {
         // TODO Auto-generated method stub
